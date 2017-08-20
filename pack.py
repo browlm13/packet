248fe3a915143f039b79f07e6f20ca7ee9df5965
browlm13 <browlm13@gmail.com>
2017-08-20 13:42:21 -0500

    Create pack.py

---
+++ pack.py
@@ -0,0 +1,298 @@
+#python 3
+
+"""
+	
+	pack data structure
+
+
+	map: represents the tag order and byte sizes help by the pack object, used for construction
+
+		format:  [ [tag,size], ... , [tag,size] ] 	#where size is in bytes
+
+	internally the pack contains a datastructure of the following format
+
+		guts = [ [tag, data], ... , [tag, data] ] #where tag is a string and data is a bytearray
+
+
+	indexing:
+
+		-indexing with p[] (square brackets) returns a pack object with the feilds specified
+			-indexing targets tags and their index in the map structure order, not the byte number
+			exception: a single index returns a bytearray data type correspoding to the tag indexed
+
+		-the () operator will return the pack's bytearrays concatinated in order
+			-(tag,index) : aditional indexing will only return 
+				those feilds indexeds (in order indexed) as concatinated bytearray
+
+		-p.map() will return the map datastructure used for pack creation
+			-p.map(tag,index...) additional feilds will return a map datastructure 
+				of the feilds specified (in order specified)
+
+	[todo]:
+		insert
+		remove/delete/del
+		append
+		arithmatic operators: + (append),
+		clear/zero
+		start #byte number
+		end #byte number
+		which
+		random fills?
+
+		change map for existing for feild change (like payload split)
+			-only works for same size bytes
+
+"""
+
+class pack():
+
+	def guts_to_bytearray(self, guts): 
+		ba = bytearray()
+		for i in guts: ba += i[1]
+		return ba
+
+	def guts_to_map(self, guts):
+		return [ [i[0],len(i[1])] for i in guts ]
+
+	def rank(self, tag=None):
+		if tag == None:
+			return len(self)-1
+		tags, contents = zip(*self.guts)
+		return tags.index(tag)
+
+	#def remove(*tags):
+	#def start_byte(tag):
+	#def end_byte(tag):
+	#def zero(*tags=None):
+	#def instert(rank,tag,)
+
+
+	def is_guts(self, arg):
+		try:
+			tags, contents = zip(*arg)
+			for i in range(len(arg)):
+
+				#ensure tag is string
+				if type(tags[i]) != str: 
+					raise Exception("Error: Tags must be type str.")
+
+				#ensure that contents are of type bytearry
+				if type(contents[i]) != bytearray: 
+					raise Exception("Error: Guts must contain bytearray as content.")
+
+			#ensure tags are unique
+			if len(set(tags)) != len(tags): 
+				raise Exception("Error: Tags must be unique.")	
+		except: return False
+		return True
+
+
+	def is_map(self, arg):
+		""" Ensure map formatting """
+		try:
+			tags, sizes = zip(*arg)
+			for i in range(len(arg)):
+
+				#ensure tag is string
+				if type(tags[i]) != str: 
+					raise Exception("Error: Tags must be type str.")
+
+				#ensure size is int and greater than 0
+				if (type(sizes[i]) != int) or (sizes[i] <= 0): 
+					raise Exception("Error: Sizes must be int > 0.")
+
+			#ensure tags are unique
+			if len(set(tags)) != len(tags): 
+				raise Exception("Error: Tags must be unique.")	
+		except: return False
+		return True
+
+	#return map comprised of entire map or selected segments
+	def map(self, *indexs):
+
+		if len(indexs) == 0:		#.map() -> returns entire map
+			return self.guts_to_map(self.guts)
+
+		guts = list(filter(lambda i: (i[0] in indexs) or (self.rank(i[0]) in indexs), self.guts))
+		return self.guts_to_map(guts)
+
+	#remap -- takes new map of diffrent size
+	def remap(self, map):
+
+		#ennsure sizes match
+		assert len(self()) ==  sum( (len(i[1]) for i in map) )
+
+		np = pack(map)
+		np <<= self()
+		return np
+
+
+	def __init__(self, initializer):
+		""" pack takes a map, or another pack as its initializer """
+
+		if type(initializer) == pack:
+			self.guts = list(initializer.guts)
+
+		elif self.is_guts(initializer):
+			self.guts = initializer
+
+		else:
+			assert self.is_map(initializer)
+			self.guts = [ [i[0], bytearray(i[1])] for i in initializer ]
+
+
+	def __getitem__(self, indexer):
+
+		if type(indexer) == int:
+			return self.guts[indexer][1]
+
+		if type(indexer) == str:
+			return self.guts[self.rank(indexer)][1]
+
+		if type(indexer) == slice:
+			start, stop, step = indexer.start, indexer.stop, None
+
+			if type(start) == str:
+				start = self.rank(start)
+
+			if type(stop) == str:
+				stop = self.rank(stop)
+
+			if start == None:
+				start = 0
+
+			if stop == None:
+				stop = len(self.guts)
+
+			#create reduced size pack object and return
+			guts = []
+			for i in range(start,stop):
+				guts += [self.guts[i]]
+
+			return pack(guts)
+
+
+	def __setitem__(self, indexer, value_bytearray):
+
+		assert type(value_bytearray) == bytearray
+
+		if type(indexer) == int:
+			self.guts[indexer][1] = value_bytearray
+
+		if type(indexer) == str:
+			self.guts[self.rank(indexer)][1] = value_bytearray
+
+	#return bytearray with p(), or bytearray of indexed params in passed order 
+	def __call__(self, *indexs):
+		if len(indexs) == 0:
+			return self.guts_to_bytearray(self.guts)
+
+
+		selected = filter(lambda i: (i[0] in indexs) or (self.rank(i[0]) in indexs), self.guts)
+		return self.guts_to_bytearray(selected)
+
+	def __len__(self):
+		return len(self.guts)
+
+	#[todo]: take feilds as well
+	def size_bytes(self):
+		return sum( (len(i[1]) for i in self.guts) )
+
+	def __repr__(self):
+		tags, contents = zip(*self.guts)
+		sizes = [len(i) for i in contents]
+		ranks = tuple(self.rank(i) for i in tags)
+		eq0 = tuple(map(lambda i: bytearray(len(self[i])) == self[i], tags))
+
+		s = "rank:\ttag:\tsize:\tzero:\n"
+		for i in range(len(self)):
+			s += ("%d\t%s\t%d\t%r\n" % (ranks[i],tags[i],sizes[i],eq0[i]))
+
+		return s
+
+	def __bytearray__(self):
+		return self()
+
+	#label in p
+	def __contains__(self, tag):
+		if tag in [i[0] for i in self.guts]: return True
+		return False
+
+
+	#import byte array with <<= instead of [:], must be same length as guide/map
+	def __ilshift__(self, raw_data):
+		assert len(raw_data) == self.size_bytes()
+
+		#break data into chunks of content sizes and set ==
+		sizes = [len(i[1]) for i in self.guts]
+
+		index = 0
+		for i in range(len(sizes)):
+			self.guts[i][1] = raw_data[index:index+sizes[i]]
+			index += sizes[i]
+	
+
+		return self
+
+
+	#import bytearray with ^=, read until full, bytearray must be at least as long as guide/map
+	def __ixor__(self, raw_data):
+		assert len(raw_data) >= self.size_bytes()
+		return self.__ilshift__(raw_data[:self.size_bytes()])
+
+"""
+	Testing
+"""
+
+"""
+import struct
+import random
+
+#
+#	useful functions
+#
+def concat(g): 
+	r = next(g)
+	for i in g: r += i
+	return r
+
+bytearray_generator = lambda length: concat(bytearray(struct.pack('B', random.randint(0,255))) for i in range(length))
+
+
+
+#test creation
+
+m = [['t1',1],['t2',2],['t3',3], ['crc',2]]
+p = pack(m)
+
+p['crc'] = bytearray_generator(2)
+print (p)
+print (p())
+#check setting crc
+
+m2 = [['t1',1],['t2',2],['t3',3]]
+p2 = pack(m2)
+p2 <<= p[:'crc']()
+print (p2)
+print (p2())
+
+#or
+p2 = p[:'crc']
+print (p2)
+print (p2())
+
+
+#new feilds
+m1 = [['t1',2],['t2',2],['t3',2], ['crc',2]]
+p1 = pack(m1)
+p1['t2'] = bytearray_generator(2)
+
+m2 = [['t1',2],['t2a',1],['t2b',1],['t3',2], ['crc',2]]
+p2 = pack(m2)
+p2 <<= p1()
+
+print(p1)
+print(p2)
+
+###!!! or change just the map
+"""
